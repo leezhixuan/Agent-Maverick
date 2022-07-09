be9930403184a6b7477a8e29f0ef113237cac139
Xun Yi <xunyi.work@gmail.com>
2022-07-09 18:59:28 +0800

    Second Commit

--- telegramBot/telegramBot.py
+++ telegramBot/telegramBot.py
@@ -4,12 +4,12 @@ import telebot
 from telebot import types
 import requests
 
-
-
 load_dotenv()
 
 APIKey = os.getenv('APIKey')
 bot = telebot.TeleBot(APIKey)
+
+messageIdList = []
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -21,7 +21,7 @@ def start(message):
     itembtn2 = types.KeyboardButton("Decrypt")
     markup.add(itembtn1,itembtn2)
     bot.send_message(message.chat.id, "Hi! Welcome to Steganography bot. Choose an option below.", reply_markup=markup)
-   
+    messageIdList.append(message.message_id)
 
 @bot.message_handler(func=lambda m: True)
 def userOption(message):
@@ -33,55 +33,74 @@ def userOption(message):
         state = 'Encrypt'
         msg = bot.reply_to(message,"Select and send an image." )
         bot.register_next_step_handler(msg, recieveImage, state)
+        messageIdList.append(message.message_id)
     elif message.text == "Decrypt":
         state = "Decrypt"
         msg = bot.reply_to(message,"Select and send an image." )
         bot.register_next_step_handler(msg, recieveImage, state)
+        messageIdList.append(message.message_id)
     else:
         bot.send_message(message.chat.id, "Please use /start again.")
+        messageIdList.append(message.message_id)
 
     
 
 def recieveImage(message, state):
     """
-    Recieved images for both encrypt and decrypt function
+    Recieved images for both encrypt and decrypt function.
+    Need to check the output file TYPE
     """
-    if message.content_type != "photo":
-        bot.send_message(message.chat.id, "Wrong image file, please select and send an image Make sure you send it the quick way.")
+    if message.content_type != "document":
+        bot.send_message(message.chat.id, "Wrong image file, please select and send an image as a file.")
         bot.register_next_step_handler(message,recieveImage)
-    elif message.content_type == "photo":
-        file_id = message.photo[-1].file_id
+        messageIdList.append(message.message_id)
+    elif message.content_type == "document":
+
+        file_id = message.document.file_id
         file_info = bot.get_file(file_id)
         file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(APIKey, file_info.file_path))
-        filename = "photosIn/" + file_id + '.jpg'
+        filename = "telegramBot/photosIn/" + file_id 
         with open(filename, 'wb') as f:
             f.write(file.content)
 
         if state == "Encrypt":
             msg = bot.reply_to(message, "Photo recieved. Please key in your message.")
             bot.register_next_step_handler(msg, encryptImage, filename)
+            messageIdList.append(message.message_id)
         if state == "Decrypt":
             msg = bot.reply_to(message, "Photo recieved.")
-            bot.register_next_step_handler(msg, decryptMessage)
+            bot.register_next_step_handler(msg, decryptMessage, filename)
+            messageIdList.append(message.message_id)
 
 
 def encryptImage(message, filename):
     """
     Gets both message to be encrypted and image, returns the image.
     """
+    imageFile = open(filename, 'rb')
     bot.send_message(message.chat.id, f"Your message to be encrypted is {message.text}, and the photo is the following.")
-    bot.send_photo(message.chat.id, photo=open(filename, "rb"))
+    bot.send_document(message.chat.id, imageFile) #Sents encrypted photo back
+    messageIdList.append(message.message_id)
     #Write function for encryption
+
+    clearChat(message)
     
 def decryptMessage(message,filename):
     """
     Need to write a function that decrypts the thing and sends it back.
     """
     output = "Test output"
-    bot.send_message(message.chat.id, f"Decrypted Message: {output}")
+    bot.send_message(message.chat.id, f"Decrypted Message: {output}") #Decrypted message
+    messageIdList.append(message.message_id) 
 
 
-
+@bot.message_handler(commands=['delete'])
+def clearChat(message):
+    """
+    Telegram API does not allow you to clear chat with a bot, hence this just a reminder to clear the chat for the user.
+    """
+    for id in messageIdList:
+        bot.delete_message(message.chat.id,id)
 # @bot.message_handler(commands=['privKey'])
 # def getPrivKey(message):
 #     privKey = message.text.split()[1]
