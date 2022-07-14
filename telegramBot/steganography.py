46cc2d9f6ecb8af708ff3cb4fa4aba5ee12bebba
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-14 11:27:08 +0800

    Remove dead import

--- telegramBot/steganography.py
+++ telegramBot/steganography.py
@@ -3,7 +3,6 @@ import random
 
 from cryptography import ascii_caesar_shift, ascii_caesar_shift_back
 from utils import convertTextToBinary
-from bitstring import BitArray
 
 def encodeText(messageToHide, filePathName): 
     if (len(messageToHide) == 0): 
